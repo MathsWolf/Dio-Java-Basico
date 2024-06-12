27d73e8d54ccaf1b5ffd73e8a34658ae8fc86dce
Matheus Chagas <matschagas@gmail.com>
2024-06-11 21:10:53 -0300

    Testes

---
+++ java-anatomia-classes/src/AulasJava/Matheus/Testes.java
@@ -0,0 +1,11 @@
+package AulasJava.Matheus;
+
+public class Testes {
+
+    public static void main(String[] args){
+
+        System.out.println("Oi, Fui executado pelo terminal!");
+        
+    }
+    
+}
